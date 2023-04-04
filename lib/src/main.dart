04de56aa06acd26ef0b904e464dad7d69bf1f21f
Barba2k2 <lorenzo.dz@hotmail.com>
2023-04-04 18:52:25 -0300

    Novas modificações

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weatherapp_starter_project/src/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +10,10 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const GetMaterialApp(
+      home: HomeScreen(),
+      title: 'Weather',
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
